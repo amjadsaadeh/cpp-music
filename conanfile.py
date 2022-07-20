039bfaaf3a12a0bb66aac840c65ea09fcb8b8f2a
Amjad Saadeh <mail@amjadsaadeh.de>
2022-07-20 16:11:17 +0200

    add first conanfile draft

---
+++ conanfile.py
@@ -0,0 +1,73 @@
+from pathlib import Path
+
+from conans import ConanFile, tools
+from conan.tools.cmake import CMake, CMakeToolchain
+from conan.tools.layout import cmake_layout
+
+class ModelRuntimeLibConan(ConanFile):
+    # Package and Src information
+    name = "cppMUSIC"
+    url = "https://github.com/amjadsaadeh/cpp-musict"
+    version = "0.0.1"
+
+    license = "MIT License"
+    description = "See README.md"
+    author = "Amjad Saadeh <mail@amjadsaadeh.de>"
+
+    # Binary relevant attributes
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps"
+
+    exports_sources = "src/*"
+
+    # scm = {
+    #     "type": "git",
+    #     "url": "auto",
+    #     "revision": "auto",
+    #     "subfolder": "."
+    # }
+
+    def requirements(self):
+        self.requires("eigen/3.4.0")
+
+    def build_requirements(self):
+        self.build_requires("cmake_installer/3.15.1@conan/stable")
+        self.build_requires("catch2/2.9.2@catchorg/stable")
+
+    # http://docs.conan.io/en/latest/reference/conanfile/methods.html#build
+    def build(self):
+        cmake = CMake(self)
+        # cmake.definitions["WITH_UTIL"] = self.options.with_util
+        cmake.configure(source_folder="src")
+        cmake.build()
+        # cmake.parallel = False
+        if not tools.cross_building(self.settings):
+            cmake.test(output_on_failure=True)
+    
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+    
+    def layout(self):
+        cmake_layout(self)
+
+        self.cpp.source.includedirs = ['include']
+
+        build_type = self.settings.get_safe("build_type", default="Release")
+        build_folder = Path('build') / build_type
+        self.folders.build = str(build_folder)
+        self.folders.generators = str(build_folder / "conan")
+
+    # http://docs.conan.io/en/latest/reference/conanfile/methods.html#package
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    # http://docs.conan.io/en/latest/reference/conanfile/methods.html#package-info
+    def package_info(self):
+        self.cpp_info.libs.append('ModelRuntimeLib')
+
+    # http://docs.conan.io/en/latest/reference/conanfile/methods.html#package-id
+    def package_id(self):
+        for k in self.deps_cpp_info.deps:
+            self.info.requires[k].full_package_mode()
