634b1a5291f5f846b3d9834fbd5729ccd56cc2e0
Amjad Saadeh <mail@amjadsaadeh.de>
2023-01-09 10:56:33 +0100

    fix build type for modeen conan

--- build.sh
+++ build.sh
@@ -17,8 +17,8 @@ then
     exit 1
 fi
 
-conan install --build_type=$BUILD_TYPE --profile:host=default --profile:build=default -of $BUILD_DIR --build=missing .
+conan install -b $BUILD_TYPE --profile:host=default --profile:build=default -of $BUILD_DIR --build=missing .
 
 cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan/conan_toolchain.cmake
 
-cmake --build $BUILD_DIR --config $BUILD_TYPE
+cmake --build $BUILD_DIR --config $BUILD_TYPE --verbose
