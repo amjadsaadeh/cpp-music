b7ab449f134579dc5ce5a6016de688a30fc9cad6
Amjad Saadeh <amjad.saadeh@cariad.technology>
2023-01-09 15:47:48 +0100

    correct installl folder

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ then
     exit 1
 fi
 
-conan install -b $BUILD_TYPE --profile:host=default --profile:build=default -of $BUILD_DIR --build=missing .
+conan install -b $BUILD_TYPE --profile:host=default --profile:build=default -if $BUILD_DIR -of $BUILD_DIR --build=missing .
 
 cmake -B $BUILD_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_TOOLCHAIN_FILE=$BUILD_DIR/conan/conan_toolchain.cmake
 
